12cd39118c0ab05d07cd34f80ec2dd24e2d1af62
Naveen Kumar Dasari <dasarinaveenkumar277@gmail.com>
2026-06-19 11:31:16 +0530

    Added new file

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ApexPlanet Internship</title>
 
-echo "Hello ApexPlanet Internship!";
+    <!-- Link CSS File -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-?>
+    <div class="container">
+        <?php
+        echo "<h1>Hello ApexPlanet Internship!</h1>";
+        ?>
+    </div>
+
+</body>
+</html>
